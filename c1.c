4ca4d1a694546491ed9699cc8cd9ab109e97e963
Defect886 <tasqoq@gmail.com>
2026-08-14 23:46:45 +0600

    chamged c1

--- c1.c
+++ c1.c
@@ -3,7 +3,7 @@ int main(){
     int h;
 
     for(int i=0; i<4; i++){
-        printf("Enter your first hexadecimal character:\n");
+        printf("Enter your %d hexadecimal character:\n", i+1);
         scanf(" %x", &h);
 
         printf("%d\n", h);
